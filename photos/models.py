fda6511e3f9b7e531493470547e2993877e8fc85
Beryl Onyancha <berylonyancha@gmail>
2019-07-22 10:33:27 +0300

    Create Category model

--- photos/models.py
+++ photos/models.py
@@ -5,3 +5,15 @@ class Location(models.Model):
     name = models.CharField(max_length = 30)
     def __str__(self):
         return self.name
+        
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
